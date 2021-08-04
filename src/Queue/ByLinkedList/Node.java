9e3651b0139b72bd51050d3aae34c925280530cf
namanseth01 <namanseth01@gmail.com>
2021-08-04 21:17:41 +0530

    Add the class Node in Queue.ByLinkedList package

---
+++ src/Queue/ByLinkedList/Node.java
@@ -0,0 +1,18 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Naman Seth (namanseth01)
+ *   Date: 04/08/21
+ *   Time: 9:17 PM
+ *   File: Node.java
+ */
+
+package Queue.ByLinkedList;
+
+public class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        next = null;
+    }
+}
