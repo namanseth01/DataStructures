36d84eff610e2e0b4d0a977370187836a1a4f0aa
namanseth01 <namanseth01@gmail.com>
2021-08-06 23:26:23 +0530

    Add the code in class Main in Sorting.BubbleSort package

---
+++ src/Sorting/BubbleSort/Main.java
@@ -0,0 +1,17 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Naman Seth (namanseth01)
+ *   Date: 06/08/21
+ *   Time: 11:25 PM
+ *   File: Main.java
+ */
+
+package Sorting.BubbleSort;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] arr = {100,80,60,40,20,10};
+        System.out.println(Arrays.toString(BubbleSort.sort(arr)));
+    }
+}
