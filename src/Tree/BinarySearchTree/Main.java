d90c67b487dc001bfa3d140ff288242882e66f3e
namanseth01 <namanseth01@gmail.com>
2021-08-08 21:55:59 +0530

    Add the code in class Main in Tree.BinarySearchTree package

---
+++ src/Tree/BinarySearchTree/Main.java
@@ -0,0 +1,28 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Naman Seth (namanseth01)
+ *   Date: 08/08/21
+ *   Time: 9:55 PM
+ *   File: Main.java
+ */
+
+package Tree.BinarySearchTree;
+
+public class Main {
+    public static void main(String[] args) {
+        Tree<Integer> integerTree = new Tree<>();
+        System.out.println(integerTree.insert(32));
+        System.out.println(integerTree.insert(13));
+        System.out.println(integerTree.insert(69));
+        System.out.println(integerTree.insert(37));
+        System.out.println(integerTree.insert(47));
+        integerTree.preOrder(integerTree.getRoot());
+        System.out.println();
+        integerTree.postOrder(integerTree.getRoot());
+        System.out.println();
+        integerTree.inOrder(integerTree.getRoot());
+        System.out.println(integerTree.search(13));
+        System.out.println(integerTree.search(69));
+        System.out.println(integerTree.search(15));
+        System.out.println(integerTree.search(67));
+    }
+}
