6b1f0843f909e7f05f3d08f762629fe3eb2a0a4e
namanseth01 <namanseth01@gmail.com>
2021-08-09 21:52:28 +0530

    Add the code in class Node in LinkedList.NumberAsLinkedList package

---
+++ src/LinkedList/NumberAsLinkedList/Node.java
@@ -0,0 +1,18 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Naman Seth (namanseth01)
+ *   Date: 09/08/21
+ *   Time: 9:52 PM
+ *   File: Node.java
+ */
+
+package LinkedList.NumberAsLinkedList;
+
+public class Node {
+    Node next;
+    int data;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
