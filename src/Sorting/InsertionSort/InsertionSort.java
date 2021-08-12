6aa60b05a53f73a6a88ae8ce01e594030948fc62
Naman Seth <naman.seth_cs19@gla.ac.in>
2021-08-12 23:02:51 +0530

    Update InsertionSort.java

--- src/Sorting/InsertionSort/InsertionSort.java
+++ src/Sorting/InsertionSort/InsertionSort.java
@@ -9,7 +9,17 @@ package Sorting.InsertionSort;
 
 public class InsertionSort {
     public static int[] sort(int[] arr) {
-        
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j >= 1; j--) {
+                if(arr[j]<arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                } else{
+                    break;
+                }
+            }
+        }
         return arr;
     }
 }
